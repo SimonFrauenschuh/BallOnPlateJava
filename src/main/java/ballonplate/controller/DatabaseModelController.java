b321578dce4dfe6494518dc1ba1ce04167c13553
SimonFrauenschuh <simon_frauenschuh@icloud.com>
2021-09-17 18:51:19 +0200

    First working version

---
+++ src/main/java/ballonplate/controller/DatabaseModelController.java
@@ -0,0 +1,80 @@
+/* Copyright (C) 2021 Simon Frauenschuh - All Rights Reserved
+ * You may use and / or modify this code in
+ * terms of private use.
+ * Any caused damage or misbehaviour of any components are
+ * under the responsibility of the user and and the editor
+ * cannot be prosecuted for it
+ */
+
+package ballonplate.controller;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import ballonplate.model.DatabaseModel;
+import ballonplate.service.DatabaseModelRegistration;
+
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://www.cdi-spec.org/faq/#accordion6
+@Model
+public class DatabaseModelController {
+
+	@Inject
+    private FacesContext facesContext;
+	
+	@Inject
+    private DatabaseModelRegistration databaseModelRegistration;
+	
+	@Produces
+	@Named
+	private DatabaseModel newDatabaseModel;
+	
+	@PostConstruct
+    public void initNewDatabaseModel() {
+		newDatabaseModel = new DatabaseModel();
+    }
+
+	// Method to be called, whenever the site is reloaded / called
+	public void startup() throws Exception {
+		
+	}
+	
+	public void register() throws Exception {
+		try {
+			databaseModelRegistration.register(newDatabaseModel);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful");
+            facesContext.addMessage(null, m);
+            initNewDatabaseModel();
+        } catch (Exception e) {
+            String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
+            facesContext.addMessage(null, m);
+        }
+	}
+	
+	private String getRootErrorMessage(Exception e) {
+        // Default to general error message that registration failed.
+        String errorMessage = "Registration failed. See server log for more information";
+        if (e == null) {
+            // This shouldn't happen, but return the default messages
+            return errorMessage;
+        }
+
+        // Start with the exception and recurse to find the root cause
+        Throwable t = e;
+        while (t != null) {
+            // Get the message from the Throwable class instance
+            errorMessage = t.getLocalizedMessage();
+            t = t.getCause();
+        }
+        // This is the root cause message
+        return errorMessage;
+    }
+}
---
+++ src/main/java/ballonplate/data/DatabaseModelRepository.java
@@ -0,0 +1,39 @@
+/* Copyright (C) 2021 Simon Frauenschuh - All Rights Reserved
+ * You may use and / or modify this code in
+ * terms of private use.
+ * Any caused damage or misbehaviour of any components are
+ * under the responsibility of the user and and the editor
+ * cannot be prosecuted for it
+ */
+
+package ballonplate.data;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import ballonplate.model.DatabaseModel;
+
+@ApplicationScoped
+public class DatabaseModelRepository {
+
+	@Inject
+	private EntityManager em;
+
+	public DatabaseModel findById(Long id) {
+		return em.find(DatabaseModel.class, id);
+	}
+	
+	public List<DatabaseModel> findAllOrderedByName() {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<DatabaseModel> criteria = cb.createQuery(DatabaseModel.class);
+		Root<DatabaseModel> databaseModel = criteria.from(DatabaseModel.class);
+		criteria.select(databaseModel).orderBy(cb.asc(databaseModel.get("id")));
+		return em.createQuery(criteria).getResultList();
+	}
+}
