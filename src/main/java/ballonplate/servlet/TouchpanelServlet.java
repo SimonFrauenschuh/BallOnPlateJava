7d08703ced699670c081d840d89fa6ea7d92184b
Simon Frauenschuh <89796114+SimonFrauenschuh@users.noreply.github.com>
2023-03-01 14:12:54 +0100

    Changed data format to json

--- src/main/java/ballonplate/servlet/TouchpanelServlet.java
+++ src/main/java/ballonplate/servlet/TouchpanelServlet.java
@@ -40,7 +40,7 @@ public class TouchpanelServlet extends HttpServlet {
     
     // Read the values from the db and pass them in the format xxxyyy
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append(Integer.toString(databaseTouchpanelRepository.getPosXReal())).append(Integer.toString(databaseTouchpanelRepository.getPosYReal()));
+		response.getWriter().append("{\"posX\":" + Integer.toString(databaseTouchpanelRepository.getPosXReal()) + ",\"posY\":" + Integer.toString(databaseTouchpanelRepository.getPosYReal()) + "}");
 	}
 	
 	// Extract the data from the POST-Request and store it into the db
